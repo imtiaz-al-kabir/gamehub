5e48ab1982fecc9c37eff135c2641c8e53586dca
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-10-23 15:08:14 +0600

    dotenv created

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -1,19 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBza4rbRl59PWxWmjBraeaVIAag717e6Zc",
-  authDomain: "gamehub-gamer.firebaseapp.com",
-  projectId: "gamehub-gamer",
-  storageBucket: "gamehub-gamer.firebasestorage.app",
-  messagingSenderId: "115659253341",
-  appId: "1:115659253341:web:e369ab7a6dc05073f60fb4"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
